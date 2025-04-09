04ccca419aa05629f12db31aba96a90cab6f92d3
Satya Ashok Dowluri <dowlurisatyaashok@gmail.com>
2025-04-09 18:11:46 -0500

    wrapper for sorting algos

--- wrapper.py
+++ wrapper.py
@@ -1,8 +1,11 @@
 import time
-from bubble.py import bubble_sort
+from bubble import bubble_sort
 
-
-inputList = ['./Half_Sorted/half_sorted_1000.txt', './Half_Sorted/half_sorted_1000.txt', './Half_Sorted/half_sorted_10000.txt', './Half_Sorted/half_sorted_50000.txt', './Half_Sorted/half_sorted_100000.txt']
+inputList = ['Half_Sorted/half_sorted_1000.txt']    
+inputList.append('Half_Sorted/half_sorted_5000.txt')
+inputList.append('Half_Sorted/half_sorted_10000.txt')
+inputList.append('Half_Sorted/half_sorted_50000.txt')
+inputList.append('Half_Sorted/half_sorted_100000.txt')
     
 inputList.append('Random/random_numbers_1000.txt')
 inputList.append('Random/random_numbers_5000.txt')
@@ -22,21 +25,25 @@ inputList.append('Sorted/Sorted_10000.txt')
 inputList.append('Sorted/Sorted_50000.txt')
 inputList.append('Sorted/Sorted_100000.txt')
 
+
 sortingAlgos=[bubble_sort]
 
-for algo
+for algo in sortingAlgos:
+    
+    print(f"{algo.__name__} times: ")
+    for path in inputList:
+        
+        with open(path,'r') as fin:
+            lines = fin.readlines()
+            
+        if("float" in path or "Sorted/Sorted" in path):
+            inputarray = [float(line) for line in lines]        
+        else:
+            inputarray = [int(line) for line in lines]
+        
+        # print(inputarray[1:10])
+        start = time.time()
+        bubble_sort(inputarray)
+        end = time.time()
 
-print("Bubble sort times: ")
-for path in inputList:
-    
-    with open(path,'r') as fin:
-        lines = fin.readlines()
-    
-    inputarray = [int(line) for line in lines]
-    
-    # print(inputarray)
-    start = time.time()
-    bubble_sort(inputarray)
-    end = time.time()
-
-    print(f"Time taken for {path.split("/")[2]}: {end-start:.4f}")
+        print(f"Time taken for {path.split("/")[1]}: {end-start:.4f}")
