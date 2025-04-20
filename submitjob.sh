ace43dda003558dd5c048cb49383e37dda1dabca
Satya Dowluri <dowlurisatyaashok@gmail.com>
2025-04-20 13:54:14 -0500

    minor

--- submitjob.sh
+++ submitjob.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-#SBATCH --job-name=sorting_benchmark_g020_gpu_1
+#SBATCH --job-name=sorting_benchmark_g016_gpu_32cores_gpu_2
 #SBATCH --partition=gpu
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=6
-#SBATCH --mem=50G
+#SBATCH --cpus-per-task=32
+#SBATCH --mem=60G
 #SBATCH --time=23:00:00
-#SBATCH --output=sorting_%j.out
-#SBATCH --error=sorting_%j.err
-#SBATCH --constraint=gpu,eth_1g,r28c1,asu_int_6_64
+#SBATCH --output=sorting_g016_newbchmks_gpu_32cores_th10%j.out
+#SBATCH --error=sorting_g016_newbchmks_gpu_32cores_th10%j.err
+#SBATCH --constraint=gpu,eth_10g,r27c1
 
 cd /moosefs/home/s265d007/projects/git2/700HPC
 
